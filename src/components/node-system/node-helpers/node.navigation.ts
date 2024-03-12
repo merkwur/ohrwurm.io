ac1a7eb4c0052240affcef4be7a95c7571b18e9a
merkwur <semihiyikalender@gmail.com>
2024-03-12 20:19:54 +0300

    handled node creation

--- src/components/node-system/node-helpers/node.navigation.ts
+++ src/components/node-system/node-helpers/node.navigation.ts
@@ -1,5 +1,5 @@
 export const positionHandler = (x: number, y: number, snapSize: number): {x: number, y: number} => { 
-  const sx = Math.floor(x / snapSize) * snapSize 
-  const sy = Math.floor(y / snapSize) * snapSize
+  const sx = Math.floor(x / snapSize) * snapSize + 5
+  const sy = Math.floor(y / snapSize) * snapSize + 5
   return {x: sx, y: sy}
 }
--- src/components/node-system/node-menu/node.menu.tsx
+++ src/components/node-system/node-menu/node.menu.tsx
@@ -1,29 +1,70 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./node.menu.scss"
 import { positionHandler } from '../node-helpers/node.navigation'
 
 const NodeMenu = () => {
   const nodeList: string[] = ["a", "b", "c", "d"]
-  const [isDragging, setIsDragging] = useState<boolean>(false)  
+  const [isDragging, setIsDragging] = useState<boolean>(false)
+  const [hollowObjectPosition, setHollowObjectPosition] = useState<{x: number, y:number}>({x: 0, y: 0})  
+  const dragPreview = useRef<HTMLDivElement | null>(null)
 
 
   const handleMouseDown: React.MouseEventHandler<HTMLDivElement> = (event) => {
+    setIsDragging(true)
     const px: number = event.clientX
     const py: number = event.clientY
     const {x, y} = positionHandler(px, py, 40)
+
+    dragPreview.current = document.createElement("div")
+    dragPreview.current.style.position = "absolute"
+    dragPreview.current.style.zIndex = "10000"
+    dragPreview.current.style.width = `${40*2-10}px`
+    dragPreview.current.style.height = `${40*2-10}px`
+    dragPreview.current.style.top = `${y}px`
+    dragPreview.current.style.left = `${x}px`
+    dragPreview.current.style.border = `1px solid #77777777`
+    dragPreview.current.style.backgroundColor = "#0707007"
+    dragPreview.current.style.backgroundSize = "cover"
+    dragPreview.current.style.pointerEvents = "none"
+    
+    const canvas = document.getElementsByClassName("canvas")
+    canvas[0].appendChild(dragPreview.current)
     console.log(`clicked on ${x} ${y}`)
-    setIsDragging(true)
   }
 
   const handleMouseMove = (event: MouseEvent) => {
-    const px: number = event.clientX
-    const py: number = event.clientY
-    const {x, y} = positionHandler(px, py, 40)
-    console.log(`clicked on ${x} ${y}`)
+    if (isDragging) {
+      const handler = setTimeout(() => {
+        const px: number = event.clientX
+        const py: number = event.clientY
+        const {x, y} = positionHandler(px, py, 40)
+        console.log(`clicked on ${x} ${y}`)
+        setHollowObjectPosition({x: x, y: y})
+      }, 20)
+      return () => clearTimeout(handler)
+    }
   }
 
-  const handleMouseUp = () => {
+  useEffect(() => {
+    if (dragPreview.current) {
+      dragPreview.current.style.left = `${hollowObjectPosition.x}px`
+      dragPreview.current.style.top = `${hollowObjectPosition.y}px`
+    }
+  }, [hollowObjectPosition])
+
+
+  const handleMouseUp = (event: MouseEvent) => {
+    const x = event.clientX
+    const y = event.clientY
     setIsDragging(false)
+    
+    if (dragPreview.current) {
+      const canvas = document.getElementsByClassName("canvas")
+      canvas[0].removeChild(dragPreview.current)
+      dragPreview.current = null; // Reset the ref
+    }
+    window.removeEventListener('mousemove', handleMouseMove)
+    window.removeEventListener('mouseup', handleMouseUp)
   }
 
   useEffect(() => {
